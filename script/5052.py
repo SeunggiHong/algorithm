15fe8bbca4c9e98c74e761df90ea12f8b3542fe5
seunggi <hsk1533@naver.com>
2024-10-26 17:56:43 +0900

    add

---
+++ script/5052.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+for _ in range(int(input().rstrip())):
+    number = []
+    N = int(input().rstrip())
+    
+    for _ in range(N):
+        number.append(input().rstrip())
+    number.sort()
+    answer = "YES"
+
+    for n in range(len(number) - 1):
+        if number[n] == number[n+1][:len(number[n])] :
+            answer = "NO"
+            break
+
+    print(answer)
